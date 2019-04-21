23a2202e1d8ddb3dbb1877bc771ac296cd7070b0
Jayon Kim <jpkim921@gmail.com>
2019-04-21 15:10:57 -0400

    done prac

--- src/Cell.js
+++ src/Cell.js
@@ -1,19 +1,31 @@
 import React, { Component } from 'react';
 
 export default class Cell extends Component {
-  
+
   constructor(props) {
     super(props)
     this.state = {
       color: this.props.color
     }
   }
-  
+
+
+  // getSelectedColor was passed in from the parent (Matrix) as a prop
+  handleClick = () => {
+    const newColor = this.props.getSelectedColor();
+    this.setState({
+      color: newColor
+    })
+  }
+
+
   render() {
     return (
-      <div className="cell" style={{backgroundColor: this.state.color}}>
+      <div className="cell"
+          style={{backgroundColor: this.state.color}}
+          onClick={this.handleClick}>
       </div>
     )
   }
-  
+
 }
--- src/Matrix.js
+++ src/Matrix.js
@@ -7,21 +7,36 @@ export default class Matrix extends Component {
 
   constructor() {
     super()
+    this.state = {
+      selectedColor: '#FFF'
+    }
   }
 
   genRow = (vals) => (
-    vals.map((val, idx) => <Cell key={idx} color={val} />)
+    vals.map((val, idx) => <Cell key={idx} color={val} getSelectedColor={this.getSelectedColor} />)
   )
 
   genMatrix = () => (
     this.props.values.map((rowVals, idx) => <div key={idx} className="row">{this.genRow(rowVals)}</div>)
   )
 
+  // the color state is being held in this Matrix component.
+  // the color that was selected was passed back from ColorSelector component (child) to Matrix (parent) through a callback function
+  setSelectedColor = (newColor) => {
+    this.setState(
+      {selectedColor: newColor}
+    )
+  }
 
+  // this is the additional method to grab the color so that we can pass it to the Cell component
+  getSelectedColor = () => {this.state.selectedColor}
+
+
+  // passing in the selectedColor method as a prop into the ColorSelector component
   render() {
     return (
       <div id="app">
-        <ColorSelector />
+        <ColorSelector setSelectedColor = {this.setSelectedColor}/>
         <div id="matrix">
           {this.genMatrix()}
         </div>
